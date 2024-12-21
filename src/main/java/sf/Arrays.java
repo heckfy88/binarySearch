ee751634fe6f0935c3b1623a509b99e6e1644eeb
Ruslan <54860448+heckfy88@users.noreply.github.com>
2024-12-21 17:40:57 +0300

    BS-1: realized

---
+++ src/main/java/sf/Arrays.java
@@ -0,0 +1,212 @@
+package sf;
+
+import java.util.Comparator;
+
+public class Arrays {
+    public static int binarySearch(byte[] a, byte key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    public static int binarySearch(byte[] a, int fromIndex, int toIndex, byte key) {
+        checkRange(fromIndex, toIndex, a.length);
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            byte midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else if (midVal > key) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    public static int binarySearch(char[] a, char key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    public static int binarySearch(char[] a, int fromIndex, int toIndex, char key) {
+        checkRange(fromIndex, toIndex, a.length);
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            char midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else if (midVal > key) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    public static int binarySearch(double[] a, double key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    public static int binarySearch(double[] a, int fromIndex, int toIndex, double key) {
+        checkRange(fromIndex, toIndex, a.length);
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            double midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else if (midVal > key) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    public static int binarySearch(float[] a, float key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    public static int binarySearch(float[] a, int fromIndex, int toIndex, float key) {
+        checkRange(fromIndex, toIndex, a.length);
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            float midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else if (midVal > key) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    public static int binarySearch(int[] a, int key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    public static int binarySearch(int[] a, int fromIndex, int toIndex, int key) {
+        checkRange(fromIndex, toIndex, a.length);
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else if (midVal > key) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    public static int binarySearch(long[] a, long key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    public static int binarySearch(long[] a, int fromIndex, int toIndex, long key) {
+        checkRange(fromIndex, toIndex, a.length);
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            long midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else if (midVal > key) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    public static int binarySearch(short[] a, short key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    public static int binarySearch(short[] a, int fromIndex, int toIndex, short key) {
+        checkRange(fromIndex, toIndex, a.length);
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            short midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else if (midVal > key) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    public static <T> int binarySearch(T[] a, T key, Comparator<? super T> c) {
+        return binarySearch(a, 0, a.length, key, c);
+    }
+
+    public static <T> int binarySearch(T[] a, int fromIndex, int toIndex, T key, Comparator<? super T> c) {
+        checkRange(fromIndex, toIndex, a.length);
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            T midVal = a[mid];
+            int cmp = c.compare(midVal, key);
+
+            if (cmp < 0) {
+                low = mid + 1;
+            } else if (cmp > 0) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    private static void checkRange(int fromIndex, int toIndex, int size) {
+        if (fromIndex < 0) {
+            throw new IllegalArgumentException("fromIndex < 0");
+        } else if (fromIndex > toIndex) {
+            throw new IllegalArgumentException("fromIndex > toIndex");
+        }
+
+        if (fromIndex > size) {
+            throw new ArrayIndexOutOfBoundsException("fromIndex > size");
+        } else if (toIndex > size) {
+            throw new ArrayIndexOutOfBoundsException("toIndex > size");
+        }
+    }
+}
